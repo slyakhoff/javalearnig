e9d1f41a8e08bcd72401853ecc81dd509727e2cc
slyakhoff <slyakhoff@gmail.com>
2017-11-08 17:40:29 +0300

    Чтение из консоли

---
+++ src/commonpackage/StdRead.java
@@ -0,0 +1,9 @@
+package commonpackage;
+import java.util.*;
+public class StdRead {
+    public static void main(String[] args){
+        Scanner Scan = new Scanner(System.in);
+        String line = Scan.nextLine();
+        System.out.println(line);
+    }
+}
